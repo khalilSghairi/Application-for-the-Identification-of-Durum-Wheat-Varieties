144138b16988a5e23786b254f0fc0b28c5b795a8
Khalil Sghairi <khalilsghairi03@gmail.com>
2024-04-13 16:01:52 +0100

    modif

--- resources/views/GérerUtilisateur/display_user.blade.php
+++ resources/views/GérerUtilisateur/display_user.blade.php
@@ -18,6 +18,7 @@
 
 						<div class="row">
 							<div class="col-12">
+                               {{--  <div class="col-12 255"> --}}
 								<div class="card">
 									<div class="card-header">
 										<div class="card-title">Liste Utilisateur</div>
